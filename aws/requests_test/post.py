6471144fcf053d089613588bc052656579729ad9
Aaron Reed <aaron73@stanford.edu>
2022-06-08 21:47:39 -0500

    cleanup post.py

--- aws/requests_test/post.py
+++ aws/requests_test/post.py
@@ -3,12 +3,19 @@ import requests
   
 # defining the api-endpoint 
 URL = "http://ec2-35-175-109-209.compute-1.amazonaws.com/"
-  
-dictToSend = {f'very{i}': f'long{i}' for i in range(5)}
-  
+
+# correctly formed dict
+dictToSend = {'ipfs_url': '123', 'wallet_address': '456'}
+
+# incorrectly formed dict; use to test server's error handling
+# dictToSend = {1:2, 3:4}
+
 # sending post request and saving response as response object
-# r = requests.post(url = API_ENDPOINT, data = data)
 r = requests.post(URL, json=dictToSend)
 print(r.text)
+
 # extracting response text 
-print(f"dict from server: {r.json()}")
+try:
+    print(f"dict from server: {r.json()}")
+except(requests.exceptions.JSONDecodeError):
+    print("Response was not in pure JSON form")
